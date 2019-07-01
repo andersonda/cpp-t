0524ddd1fc048ad41267bb4e17888702ccd26601
David Anderson <andersonda8109@gmail.com>
2019-06-30 18:23:38 -0700

    added test files from clair

--- src/test/variables.cpp
+++ src/test/variables.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() {
     int a = 10;
     int b = 100;
-    int c {10};
+    int c = 10;
     float d = 100.1020;
     double e = 1999.99999;
-    string d {"hello"};
+    string f = "hello";
 
-    cout << d;
+    cout << "a: " << a << " b: " << b << " c: " << c << " d: " << d << " e: " << e << " f: " << f;
 
     return 0;
 }
